2fd798970a1fc695371a067888859152ef439c6c
Linus <72553793+linus-linus@users.noreply.github.com>
2022-04-29 17:43:59 +0200

    mye

---
+++ Migrations/20220429132458_InitialCreate26.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ArmyAPI.Migrations
+{
+    public partial class InitialCreate26 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "Soldier");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Image",
+                table: "Soldier",
+                type: "TEXT",
+                nullable: true);
+        }
+    }
+}
---
+++ Migrations/20220429150506_InitialCreate29.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ArmyAPI.Migrations
+{
+    public partial class InitialCreate29 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
