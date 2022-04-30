06850161df7273325e4daca3c76d649272d74744
Andrea Kjellvang <andrea.r.k@hotmail.com>
2022-04-30 16:22:59 +0200

    category i vehicle

---
+++ Migrations/20220430131305_InitialCreate31.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ArmyAPI.Migrations
+{
+    public partial class InitialCreate31 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Category",
+                table: "Vehicle",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Category",
+                table: "Vehicle");
+        }
+    }
+}
